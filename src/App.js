1ff0975e818a88de9fce47601f0c729d447efaa0
Ethan Aldrich <e.aldrich@budgetlasvegas.com>
2026-09-14 18:55:45 -0700

    Replace React logo with Propeller Aero logo and link to DirtMate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+const logo =
+  'https://cdn-ikponof.nitrocdn.com/vGqfYAGlOLDkYkJqZhYIYKEsibdbZnkc/assets/images/optimized/rev-f684a87/www.propelleraero.com/wp-content/uploads/2023/05/footer-logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Propeller Aero logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about DirtMate
         </a>
       </header>
     </div>
